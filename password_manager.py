5ede5f90d9075b548f8303a63f0bd4fb01219e61
javier <1996javiermejia@gmail.com>
2024-03-08 15:34:30 -0600

    password manager program, a little difficult, not understanding all of it, still good for practice, encoding and decoding

---
+++ password_manager.py
@@ -0,0 +1,47 @@
+#cryptography module must be installed
+from cryptography.fernet import Fernet
+'''
+#function to create an encription key, it uses the criptography 
+def write_key():
+    key = Fernet.generate_key()
+    with open('key.key','wb') as key_file:
+        key_file.write(key)
+write_key()
+'''
+#master password: yes
+def load_key():
+    file = open('key.key', 'rb')
+    key = file.read()
+    file.close()
+    return key
+
+master_pwd = input('what is the master password?')
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
+
+def view():
+    with open('passwrd.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split('/')
+            print('User:', user,'Password:', fer.decrypt(passw.encode())) #over here if you want to view the password and not the encrypted, you decode it, .decode()
+
+def add():
+    name=input('Account Name: ')
+    pwd = input('Password: ')
+
+    with open('passwrd.txt', 'a') as f:
+        f.write(name + '/' + fer.encrypt(pwd.encode()).decode() + '\n')
+
+
+while True:
+    mode = input('would you like to add a new password, view the existing ones or quit? view/add/q: ')
+    if mode == 'q':
+        break
+
+    if mode == 'view':
+        view()
+    elif mode == 'add':
+        add()
+    else:
+        print('invalid mode.')
